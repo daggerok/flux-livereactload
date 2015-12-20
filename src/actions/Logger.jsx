3f186d501b5c029564a8019049d0b7b15bd37409
Maksim Kostromin <daggerok@gmail.com>
2015-12-20 16:24:04 +0200

    add logger

---
+++ src/actions/Logger.jsx
@@ -0,0 +1,20 @@
+const Reflux = require('reflux'),
+
+Logger = Reflux.createAction(),
+
+Storage = Reflux.createStore({
+  init() {
+    this.listenTo(Logger, this.log)
+  },
+
+  log(data) {
+    this.trigger(data)
+  }
+})
+
+Storage.listen(data => {
+  console.log(data)
+})
+
+
+module.exports = Logger
--- src/entry.jsx
+++ src/entry.jsx
@@ -1,5 +1,13 @@
 const React = require('react'),
 ReactDOM = require('react-dom'),
-Main = require('./components/Main.jsx')
+Main = require('./components/Main.jsx'),
+log = require('./actions/Logger.jsx')
+
+log('hello, reflux!')
+
+log({
+  info: 'bootstrapping components...',
+  component: Main
+})
 
 ReactDOM.render(<Main className="container panel wrapper"/>, document.getElementById('view'))
